fa4461592942636a5602c7ac761ed3820218dfa3
TimeTrialhub <110519846+RyaaV2@users.noreply.github.com>
2026-08-27 20:27:57 +0800

    Update config.lua

--- strategies/hardcore/config.lua
+++ strategies/hardcore/config.lua
@@ -2,6 +2,7 @@
 -- Edit matchmaking, required towers, map priority, and map strategy paths here.
 
 return {
+    MatchTimeoutMinutes = 10,
     MilestoneBehavior = "DirectMatchmaking",
 
     CountLoseGameOver = true,
