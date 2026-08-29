172795ccc1d8c5bec39e5630d55dc01be70f0f98
TimeTrialhub <110519846+RyaaV2@users.noreply.github.com>
2026-08-29 13:04:01 +0800

    Delete strategies directory

--- strategies/lategrind.lua
+++
@@ -1,7 +0,0 @@
--- Late-game resource grind controller.
--- Change only this file when you want the late grind to use another route/target.
-
-return {
-    Route = "Fallen.Late",
-    CoinTarget = 10000
-}
