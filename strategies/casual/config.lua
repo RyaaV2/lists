3105c7c5507924e1d193bf4038736113409beebf
TimeTrialhub <110519846+RyaaV2@users.noreply.github.com>
2026-08-30 10:58:26 +0800

    Update config.lua

--- strategies/casual/config.lua
+++ strategies/casual/config.lua
@@ -10,10 +10,12 @@ return {
         "Dead Ahead",
         "Lay By",
         "Mason Arch",
+        "Black Spot Exchange",
     },
     Maps = {
         ["Mason Arch"] = "strategies/casual/mason_arch.lua",
         ["Dead Ahead"] = "strategies/casual/dead_ahead.lua",
-        ["Lay By"] = "strategies/casual/lay_by.lua"
+        ["Lay By"] = "strategies/casual/lay_by.lua",
+        ["Black Spot Exchange"] = "strategies/casual/black_spot_exchange.lua"
     }
 }
