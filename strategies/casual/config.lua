b877e5244ccb99f56957740a1dd4e898d1178ef1
TimeTrialhub <110519846+RyaaV2@users.noreply.github.com>
2026-08-30 09:33:57 +0800

    Update config.lua

--- strategies/casual/config.lua
+++ strategies/casual/config.lua
@@ -1,7 +1,15 @@
 return {
     MatchTimeoutMinutes = 15,
     Matchmaking = {Difficulty = "Casual", Type = "survival"},
-    RequiredTowers = {},
-    Priority = {},
-    Maps = {}
+    RequiredTowers = {
+        "Militant",
+        "Commander",
+        "Assassin"
+    },
+    Priority = {
+        "Mason Arch"
+    },
+    Maps = {
+        ["Mason Arch"] = "strategies/casual/mason_arch.lua",
+    }
 }
