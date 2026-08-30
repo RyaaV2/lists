eae00d5e63aa1249c544a4fae340f214a5e0d2ba
TimeTrialhub <110519846+RyaaV2@users.noreply.github.com>
2026-08-30 11:36:06 +0800

    Add 'Construction Crazy' map to casual config

--- strategies/casual/config.lua
+++ strategies/casual/config.lua
@@ -11,11 +11,13 @@ return {
         "Lay By",
         "Mason Arch",
         "Black Spot Exchange",
+        "Construction Crazy",
     },
     Maps = {
         ["Mason Arch"] = "strategies/casual/mason_arch.lua",
         ["Dead Ahead"] = "strategies/casual/dead_ahead.lua",
         ["Lay By"] = "strategies/casual/lay_by.lua",
-        ["Black Spot Exchange"] = "strategies/casual/black_spot_exchange.lua"
+        ["Black Spot Exchange"] = "strategies/casual/black_spot_exchange.lua",
+        ["Construction Crazy"] = "strategies/casual/construction_crazy.lua"
     }
 }
