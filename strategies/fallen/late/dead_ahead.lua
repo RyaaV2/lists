7dfe9757f37dd54acad3efcee94bc5c4e3a1986c
TimeTrialhub <110519846+RyaaV2@users.noreply.github.com>
2026-09-01 05:12:48 +0800

    Update dead_ahead.lua

--- strategies/fallen/late/dead_ahead.lua
+++ strategies/fallen/late/dead_ahead.lua
@@ -56,6 +56,7 @@ TDS:Upgrade(7, 2)
 TDS:UpgradeTimes(6, 2)
 
 TDS:Place("Mercenary Base", -12.744829177856445, 0.999992847442627, 43.93040084838867, true)
+TDS:UpgradeTimes(9, 6)
 
 TDS:UpgradeTimes(5, 2)
 TDS:SetOption(5, "Trap", "Bear Traps")
@@ -67,6 +68,10 @@ TDS:UpgradeTimes(3, 3)
 TDS:SetOption(3, "Trap", "Bear Traps")
 
 TDS:UpgradeTimes(2, 3)
+TDS:SetOption(2, "Trap", "Bear Traps")
+
+TDS:Place("Mercenary Base", -12.744829177856445, 0.999992847442627, 43.93040084838867, true)
+TDS:UpgradeTimes(10, 6)
 
 TDS:LoopAbility(7, "Hologram Tower", {
     towerPosition = Vector3.new(-12.744829177856445, 25.999992847442627, 43.93040084838867),
