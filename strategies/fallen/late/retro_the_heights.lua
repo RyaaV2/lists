f67d4315626cc8651f682d4a4bd202be339023e4
TimeTrialhub <110519846+RyaaV2@users.noreply.github.com>
2026-09-01 05:47:03 +0800

    Update retro_the_heights.lua

--- strategies/fallen/late/retro_the_heights.lua
+++ strategies/fallen/late/retro_the_heights.lua
@@ -57,6 +57,7 @@ TDS:Upgrade(7, 2)
 TDS:UpgradeTimes(6, 2)
 
 TDS:Place("Mercenary Base", -12.654920578002930, 1.562506079673767, -29.426273345947266, true)
+TDS:UpgradeTimes(9, 6)
 
 TDS:UpgradeTimes(5, 2)
 TDS:SetOption(5, "Trap", "Bear Traps")
@@ -68,8 +69,12 @@ TDS:UpgradeTimes(3, 3)
 TDS:SetOption(3, "Trap", "Bear Traps")
 
 TDS:UpgradeTimes(2, 3)
+TDS:SetOption(2, "Trap", "Bear Traps")
+
+TDS:Place("Mercenary Base", -12.654920578002930, 1.562506079673767, -29.426273345947266, true)
+TDS:UpgradeTimes(10, 6)
 
 TDS:LoopAbility(7, "Hologram Tower", {
-    towerPosition = Vector3.new(-12.654920578002930, 26.562506079673767, -29.426273345947266),
+    towerPosition = Vector3.new(-12.654920578002930, 1.562506079673767, -29.426273345947266),
     towerToClone = 6
 })
