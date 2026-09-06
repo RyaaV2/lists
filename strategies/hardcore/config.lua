93e09c8cf0eeb276563d92768fc47a06571975c4
TimeTrialhub <110519846+RyaaV2@users.noreply.github.com>
2026-09-07 06:38:50 +0800

    Update config.lua

--- strategies/hardcore/config.lua
+++ strategies/hardcore/config.lua
@@ -5,6 +5,7 @@ return {
     MatchTimeoutMinutes = 10,
 
     CountLoseGameOver = true,
+    CompletionBehavior = "ReturnToLobby",
 
         Matchmaking = {Difficulty = "Easy", Type = "hardcore", ExpectedDifficulty = "Hardcore"},
         RequiredTowers = {"Pyromancer", "Hunter"},
