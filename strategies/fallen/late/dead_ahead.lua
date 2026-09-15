dacc491e782a6504a70d184c2329eb6def55ef9c
TimeTrialhub <110519846+RyaaV2@users.noreply.github.com>
2026-09-16 06:31:37 +0800

    Update VoteSkip parameters in dead_ahead.lua

--- strategies/fallen/late/dead_ahead.lua
+++ strategies/fallen/late/dead_ahead.lua
@@ -11,7 +11,9 @@ TDS:GameInfo("Dead Ahead", {
     "Fog"
 })
 
-TDS:VoteSkip()
+TDS:VoteSkip(1,10)
+TDS:VoteSkip(13,30)
+TDS:VoteSkip(32,36)
 TDS:Mercenary(80, 1)
 
 TDS:Place("Trapper", 1.2970037460327148, 1.0000042915344238, -26.240280151367188, true)
