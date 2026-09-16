a104f7e8e92fb85e3d98711a9c3894f815fa42d6
TimeTrialhub <110519846+RyaaV2@users.noreply.github.com>
2026-09-16 09:34:26 +0800

    Change CountLoseGameOver to false

--- strategies/molten/config.lua
+++ strategies/molten/config.lua
@@ -5,7 +5,7 @@ return {
     MatchTimeoutMinutes = 15,
     MilestoneBehavior = "ReturnToLobby",
 
-    CountLoseGameOver = true,
+    CountLoseGameOver = false,
 
         Matchmaking = {Difficulty = "Molten", Type = "survival"},
         RequiredTowers = {"Soldier"},
