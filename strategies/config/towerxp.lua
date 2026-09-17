d30e18227da416b085cc4e6af51c20a727469d5f
TimeTrialhub <110519846+RyaaV2@users.noreply.github.com>
2026-09-17 19:27:57 +0800

    Update towerxp.lua

--- strategies/config/towerxp.lua
+++ strategies/config/towerxp.lua
@@ -1,45 +1,14 @@
 return {
     MinimumLevel = 50,
-    
+
     Towers = {
-        {
-            Name = "Scout",
-            GameName = "Scout"
-        },
-
-        {
-            Name = "Operator",
-            GameName = "EvolvedOperator"
-        },
-
-        {
-            Name = "Minigunner",
-            GameName = "Minigunner"
-        },
-
-        {
-            Name = "Juggernaut",
-            GameName = "EvolvedJuggernaut"
-        },
-
-        {
-            Name = "Crook Boss",
-            GameName = "Crook Boss"
-        },
-
-        {
-            Name = "Kingpin",
-            GameName = "EvolvedKingpin"
-        },
-
-        {
-            Name = "Shotgunner",
-            GameName = "Shotgunner"
-        },
-
-        {
-            Name = "Enforcer",
-            GameName = "EvolvedEnforcer"
-        }
+        {Name = "Scout", GameName = "Scout"},
+        {Name = "Operator", GameName = "EvolvedOperator"},
+        {Name = "Minigunner", GameName = "Minigunner"},
+        {Name = "Juggernaut", GameName = "EvolvedJuggernaut"},
+        {Name = "Crook Boss", GameName = "Crook Boss"},
+        {Name = "Kingpin", GameName = "EvolvedKingpin"},
+        {Name = "Shotgunner", GameName = "Shotgunner"},
+        {Name = "Enforcer", GameName = "EvolvedEnforcer"}
     }
 }
