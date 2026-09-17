665aa85a449f7531ecb59cee9fa530287cbde655
TimeTrialhub <110519846+RyaaV2@users.noreply.github.com>
2026-09-17 20:19:09 +0800

    Create towerxp.lua

---
+++ strategies/towerxp.lua
@@ -0,0 +1,21 @@
+return function(TDS, selectedTower)
+    selectedTower = tostring(selectedTower or "")
+
+    if selectedTower == "" or selectedTower == "None" then
+        return false
+    end
+
+    TDS:Loadout("Pyromancer", "Hunter", selectedTower, "None", "None")
+    TDS:Mode("Hardcore")
+    TDS:GameInfo("Wretched Front")
+    TDS:VoteSkip()
+    TDS:Place("Pyromancer", -5.936307430267334, 0.9551397562026978, -31.831748962402344, true)
+    TDS:Ready()
+    TDS:UpgradeTimes(1, 4)
+    for _ = 1, 4 do TDS:Place("Hunter", 2.679039716720581, 1.341203212738037, 21.30302619934082, true) end
+    TDS:WaitForWave(22)
+    for i = 1, 5 do TDS:Sell(i) end
+    TDS:Place(selectedTower, 2.679039716720581, 1.341203212738037, 21.30302619934082, true)
+
+    return true
+end
