e6c9624e83b98259fb0b3fb3adbb056ca3452460
TimeTrialhub <110519846+RyaaV2@users.noreply.github.com>
2026-09-24 23:10:56 +0800

    Use auto restart for premium currency farm

--- strategies/currency/premium/config.lua
+++ strategies/currency/premium/config.lua
@@ -7,7 +7,7 @@ return {
         Description = "Better rewards if modifiers owned",
         RouteKey = "PremiumCurrency.Coins",
         RouteConfigPath = "strategies/currency/premium/fallen_late/config.lua",
-        GameOverAction = "Rematch",
+        GameOverAction = "Restart",
 
         SupportedModifiersTrials = {
             "HiddenEnemies",
