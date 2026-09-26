2e93ecdc662a1470b803814aa54a5daa1a31a3e1
TimeTrialhub <110519846+RyaaV2@users.noreply.github.com>
2026-09-26 13:35:56 +0800

    Update timescale_ticket.lua

--- strategies/currency/timescale_ticket.lua
+++ strategies/currency/timescale_ticket.lua
@@ -11,7 +11,7 @@ return {
         ExplodingEnemies = "Exploding",
         FlyingEnemies = "Flying",
         SpeedyEnemies = "Speedy",
-        JailedTowers = "Jailed"
+        JailedToers = "Jailed"
     },
 
     Description = "Timescale Ticket Trial Farm",
