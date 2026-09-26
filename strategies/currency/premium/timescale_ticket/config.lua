78963bee1a84be867546782489918fa2a0bb0553
TimeTrialhub <110519846+RyaaV2@users.noreply.github.com>
2026-09-26 14:10:41 +0800

    Add Timescale Trial route config

---
+++ strategies/currency/premium/timescale_ticket/config.lua
@@ -0,0 +1,22 @@
+return {
+    Matchmaking = {
+        Difficulty = "Trial",
+        Type = "Trials",
+        Count = 1
+    },
+
+    Trials = {
+        FlyingEnemies = {
+            DisplayName = "Flying",
+            StrategyPath = "strategies/currency/premium/timescale_ticket/flying.lua",
+
+            RequiredTowers = {
+                "Hacker",
+                "Gatling Gun",
+                "Militant",
+                "Mercenary Base",
+                "DJ Booth"
+            }
+        }
+    }
+}
